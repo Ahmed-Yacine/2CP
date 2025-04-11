be21d3d1e66b84947e9e0799ac6ce421578b0962
Ahmed-Yacine <ay.boutera@esi-sba.dz>
2025-04-11 22:28:53 +0100

    fix photos problem

--- Utils/Cloudinary.js
+++ Utils/Cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name:"dj62xbjdj",
-  api_key: "632715116798545",
-  api_secret: "gCYSGdfvguRByBSFjw0B2xqoFQA",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
